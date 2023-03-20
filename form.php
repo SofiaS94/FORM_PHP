30c997812666ef00659c71728affe6f509d19624
Sofia Slimani <[mmeslimani.s@gmail.com] (mailto:mmeslimani.s@gmail.com)>
2023-03-21 00:20:56 +0100

    updated thanks php used if else and filter var

--- form.php
+++ form.php
@@ -1,64 +1,64 @@
 <style>
-form {
-    /* Uniquement centrer le formulaire sur la page */
-    margin: 0 auto;
-    width: 400px;
-    /* Encadré pour voir les limites du formulaire */
-    padding: 1em;
-    border: 1px solid #CCC;
-    border-radius: 1em;
-}
+    form {
+        /* Uniquement centrer le formulaire sur la page */
+        margin: 0 auto;
+        width: 400px;
+        /* Encadré pour voir les limites du formulaire */
+        padding: 1em;
+        border: 1px solid #CCC;
+        border-radius: 1em;
+    }
 
-form div+div {
-    margin-top: 1em;
-}
+    form div+div {
+        margin-top: 1em;
+    }
 
-label {
-    /* Pour être sûrs que toutes les étiquettes ont même taille et sont correctement alignées */
-    display: inline-block;
-    width: 90px;
-    text-align: right;
-}
+    label {
+        /* Pour être sûrs que toutes les étiquettes ont même taille et sont correctement alignées */
+        display: inline-block;
+        width: 90px;
+        text-align: right;
+    }
 
-input,
-textarea {
-    /* Pour s'assurer que tous les champs texte ont la même police.
+    input,
+    textarea {
+        /* Pour s'assurer que tous les champs texte ont la même police.
      Par défaut, les textarea ont une police monospace */
-    font: 1em sans-serif;
+        font: 1em sans-serif;
 
-    /* Pour que tous les champs texte aient la même dimension */
-    width: 300px;
-    box-sizing: border-box;
+        /* Pour que tous les champs texte aient la même dimension */
+        width: 300px;
+        box-sizing: border-box;
 
-    /* Pour harmoniser le look & feel des bordures des champs texte */
-    border: 1px solid #999;
-}
+        /* Pour harmoniser le look & feel des bordures des champs texte */
+        border: 1px solid #999;
+    }
 
-input:focus,
-textarea:focus {
-    /* Pour souligner légèrement les éléments actifs */
-    border-color: #000;
-}
+    input:focus,
+    textarea:focus {
+        /* Pour souligner légèrement les éléments actifs */
+        border-color: #000;
+    }
 
-textarea {
-    /* Pour aligner les champs texte multi‑ligne avec leur étiquette */
-    vertical-align: top;
+    textarea {
+        /* Pour aligner les champs texte multi‑ligne avec leur étiquette */
+        vertical-align: top;
 
-    /* Pour donner assez de place pour écrire du texte */
-    height: 5em;
-}
+        /* Pour donner assez de place pour écrire du texte */
+        height: 5em;
+    }
 
-.button {
-    /* Pour placer le bouton à la même position que les champs texte */
-    padding-left: 90px;
-    /* même taille que les étiquettes */
-}
+    .button {
+        /* Pour placer le bouton à la même position que les champs texte */
+        padding-left: 90px;
+        /* même taille que les étiquettes */
+    }
 
-button {
-    /* Cette marge supplémentaire représente grosso modo le même espace que celui
+    button {
+        /* Cette marge supplémentaire représente grosso modo le même espace que celui
      entre les étiquettes et les champs texte */
-    margin-left: .5em;
-}
+        margin-left: .5em;
+    }
 </style>
 
 
@@ -84,6 +84,7 @@ button {
     <div>
         <label for="sujet">Sujet :</label>
         <select id="sujet" name="sujet">
+            <option value=""></option>
             <option value="cours">Cours</option>
             <option value="projets">Projets</option>
             <option value="problème">Problème</option>
--- thanks.php
+++ thanks.php
@@ -1,5 +1,4 @@
 <?php
-
 $nom = $_POST['user_name'];
 $prenom = $_POST['user_surname'];
 $email = $_POST['user_email'];
@@ -7,7 +6,12 @@ $telephone = $_POST['telephone'];
 $sujet = $_POST['sujet'];
 $message = $_POST['message'];
 
-
-echo "Merci $prenom $nom de nous avoir contacté à propos de \"$sujet\".<br><br>";
-echo "Un de nos conseillers vous contactera soit à l’adresse $email ou par téléphone au $telephone dans les plus brefs délais pour traiter votre demande :<br><br>";
-echo "$message";
+if (empty($nom) || empty($prenom) || empty($email) || empty($telephone) || empty($sujet) || empty($message)) {
+    echo "Veuillez remplir tous les champs du formulaire.";
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Veuillez saisir une adresse e-mail valide.";
+} else {
+    echo "Merci $prenom $nom de nous avoir contacté à propos de \"$sujet\".<br><br>";
+    echo "Un de nos conseillers vous contactera soit à l’adresse $email ou par téléphone au $telephone dans les plus brefs délais pour traiter votre demande :<br><br>";
+    echo "$message";
+}
